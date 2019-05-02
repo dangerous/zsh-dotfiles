05bea4d83ac2ff96c6d94369cca76ee0383b04bc
David Rainsford <david@rainsford.org>
2019-05-02 10:58:55 +1000

    Inline tmuxinator scripts

--- custom/tmuxinator.zsh
+++ custom/tmuxinator.zsh
@@ -1,3 +1,31 @@
-[[ -s $HOME/.tmuxinator/scripts/tmuxinator ]] && source $HOME/.tmuxinator/scripts/tmuxinator
-[[ -s $HOME/.tmuxinator/bin/tmuxinator_completion ]] && source $HOME/.tmuxinator/bin/tmuxinator_completion
+_tmuxinator() {
+  local commands projects
+  commands=(${(f)"$(tmuxinator commands zsh)"})
+  projects=(${(f)"$(tmuxinator completions start)"})
+
+  if (( CURRENT == 2 )); then
+    _alternative \
+      'commands:: _describe -t commands "tmuxinator subcommands" commands' \
+      'projects:: _describe -t projects "tmuxinator projects" projects'
+  elif (( CURRENT == 3)); then
+    case $words[2] in
+      copy|debug|delete|open|start)
+        _arguments '*:projects:($projects)'
+      ;;
+    esac
+  fi
+
+  return
+}
+
+compdef _tmuxinator tmuxinator mux
+#alias mux="tmuxinator"
 alias mux='TERM=screen-256color tmuxinator'
+
+# Local Variables:
+# mode: Shell-Script
+# sh-indentation: 2
+# indent-tabs-mode: nil
+# sh-basic-offset: 2
+# End:
+# vim: ft=zsh sw=2 ts=2 et
